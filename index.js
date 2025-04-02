f134fe4cf90a4a4a017574de79ae0bafb8a18c2d
Pranjal Basu <pranjalbasu8@gmail.com>
2025-04-02 12:12:17 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Bye MCKV')
+  res.send('Hello MCKV')
 })
 
 app.get('/courses', function (req, res) {
